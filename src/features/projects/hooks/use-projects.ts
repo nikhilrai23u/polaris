77904fab48dd3001a65c389e139a38f9ff0b2806
nikhil rai <nikhil.rai23u@gmail.com>
2026-03-02 21:41:22 +0530

    08: ide layout

--- src/features/projects/hooks/use-projects.ts
+++ src/features/projects/hooks/use-projects.ts
@@ -6,6 +6,10 @@ import { api } from "../../../../convex/_generated/api";
 import { useAuth } from "@clerk/nextjs";
 import { Id } from "../../../../convex/_generated/dataModel";
 
+export const useProject = (projectId: Id<"projects">) => {
+    return useQuery(api.projects.getById , {id: projectId}) ; 
+};
+
 export const useProjects = () => {
     return useQuery(api.projects.get) ; 
 };
@@ -17,8 +21,6 @@ export const useProjectsPartial = (limit: number) => {
 };
 
 export const useCreateProject  = () => { 
-    const {userId} = useAuth() ; 
-
     return useMutation(api.projects.create).withOptimisticUpdate(
         (localStore , args) => {
             const existingProjects  = localStore.getQuery(api.projects.get) ; 
@@ -39,6 +41,44 @@ export const useCreateProject  = () => {
 
                 ]) 
             }
+        }
+    ) ; 
+}
+
+export const useRenameProject  = (projectId: Id<"projects">) => { 
+    return useMutation(api.projects.rename).withOptimisticUpdate(
+        (localStore , args) => {
+            const existingProject = localStore.getQuery(
+                api.projects.getById,
+                {id: projectId}
+            ) ; 
+
+            if(existingProject !== undefined && existingProject !== null) {
+                localStore.setQuery(
+                    api.projects.getById , 
+                    {id : projectId} , 
+                    {
+                        ...existingProject ,
+                        name: args.name , 
+                        updatedAt: Date.now() , 
+                    }
+                );
+            }
+
+            const existingProjects  = localStore.getQuery(api.projects.get) ; 
+
+            if(existingProjects !== undefined) {
+                localStore.setQuery(
+                    api.projects.get,
+                    {} , 
+                    existingProjects.map((project) => {
+                        return project._id === args.id
+                        ? {...project, name: args.name , updatedAt: Date.now()}
+                        : project
+                    })
+                )
+            }
+
         }
     ) ; 
 }
